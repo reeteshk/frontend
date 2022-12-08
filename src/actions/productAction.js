72e88218a0b37568b6accf2456caef6f2a4a07b1
reeteshkesarwani <kesarwani.r@northeastern.edu>
2022-12-08 09:16:24 -0500

    Update productAction.js

--- src/actions/productAction.js
+++ src/actions/productAction.js
@@ -39,10 +39,10 @@ export const getProduct =
         async (dispatch) => {
             try {
                 dispatch({ type: ALL_PRODUCT_REQUEST });
-                let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
+                let link = `https://getgrocery-api.onrender.com/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
 
                 if (category) {
-                    link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
+                    link = `https://getgrocery-api.onrender.com/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
                 }
 
                 const { data } = await axios.get(link);
